df638da30215d7db175ad41118c3b86a492bb882
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2019-10-12 20:47:57 +0200

    Check the double while cyclus in around algorithm

--- client/src/test/java/cz/xelfi/karel/KarelModelTest.java
+++ client/src/test/java/cz/xelfi/karel/KarelModelTest.java
@@ -22,6 +22,7 @@ import static junit.framework.Assert.assertNotNull;
 import net.java.html.junit.BrowserRunner;
 import net.java.html.junit.HTMLContent;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -95,4 +96,109 @@ public class KarelModelTest {
         assertEquals("hit the wall", 1, cur.getError());
     }
 
+    @Test
+    public void whileAndWhile() throws Exception {
+        String prg = "PROCEDURE right\n"
+                + "  LEFT\n"
+                + "  LEFT\n"
+                + "  LEFT\n"
+                + "END\n"
+                + "PROCEDURE rightstep\n"
+                + "  right\n"
+                + "  STEP\n"
+                + "  LEFT\n"
+                + "END\n"
+                + "PROCEDURE around\n" +
+                "  PUT\n" +
+                "  rightstep\n" +
+                "  WHILE NOT MARK\n" +
+                "    WHILE WALL\n" +
+                "      right\n" +
+                "    END\n" +
+                "    STEP\n" +
+                "    LEFT\n" +
+                "  END\n" +
+                "END\n"
+                + "";
+        Town init = new Town();
+        init.clear();
+        init.wall(4, 5);
+        init.wall(5, 5);
+      //  init.wall(6, 5);
+        init.wall(7, 5);
+        init.wall(4, 4);
+        init.wall(5, 4);
+        init.wall(6, 4);
+        init.wall(7, 4);
+        init.wall(4, 3);
+        init.wall(5, 3);
+        init.wall(6, 3);
+        init.wall(7, 3);
+        init.step();
+        init.step();
+        init.step();
+        init.step();
+        init.step();
+        init.left();
+        init.step();
+        init.step();
+        init.step();
+        
+        assertEquals("no error", 0, init.getError());
+        
+        Town end = init.clone();
+        end.put();
+        
+        TaskDescription td = new TaskDescription("xyz", "xyz", null, 0,
+            new TaskTestCase("xyz", init, null, end, prg, "current")
+        );
+        Karel km = new Karel().
+                assignTab("town").
+                assignCurrentTask(td).
+                assignSource(prg).
+                assignSpeed(10).
+                assignTasksUrl("tasks/list.js");
+        KarelModel.compileSource(km);
+        
+        Command cmd = null;
+        for (Command c : km.getCommands()) {
+            if ("around".equals(c.getName())) {
+                cmd = c;
+                break;
+            }
+        }
+        assertNotNull("around found among " + km.getCommands(), cmd);
+
+        km.setSpeed(-1);
+        assertEquals("Runs all code at once", -1, km.getSpeed());
+        KarelModel.invoke(km, cmd);
+        
+        final Town cur = km.getCurrentTask().getTests().get(0).getCurrent();
+        
+        if (cur.equals(end)) {
+            return;
+        }
+        dump(init);
+        System.err.println("----");
+        dump(cur);
+        
+        fail("Unexpected result");
+    }
+
+    private void dump(Town t) {
+        for (Row r : t.getRows()) {
+            for (Square s : r.getColumns()) {
+                if (s.getRobot() != 0) {
+                    System.err.print("" + (char)('R' + s.getMarks()));
+                } else {
+                    if (s.getMarks() == 111) {
+                        System.err.print("W");
+                    } else {
+                        System.err.print(s.getMarks());
+                    }
+                }
+            }
+            System.err.println();
+        }
+    }
 }
